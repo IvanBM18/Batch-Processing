963928859a2d294ab10367a063f0fb79600d2812
Edgarbanzo <edgarzepedau@hotmail.com>
2022-08-26 10:00:12 -0500

    Interfaz grafica Añadida, desconecte los modulos del main

--- main.py
+++ main.py
@@ -9,16 +9,31 @@
 #Añadir los procesos a la cola
 #Pantalla de ejecución de los lotes
 
-import os
-from models.queue import Queue
-from models.process import Process
+# import os
+# from models.queue import Queue
+# from models.process import Process
+#si te da error, instala pyside2 para checar si esta es pip show pyside2
+from PySide2.QtWidgets import QApplication
 
-print("Aqui ira el menu")
+from mainwindow import MainWindow 
+import sys
+
+    
+# Aplicación de Qt
+app = QApplication()
+# Se crea un botón con la palabra Hola
+window=MainWindow()
+# Se hace visible el botón
+window.show()
+# Qt loop
+sys.exit(app.exec_())
+
+# print("Aqui ira el menu")
 
 
-n = str(input("Numero de Procesos a Introducir: "))
-while (n.isdigit() == False or int(n) < 1):
-    os.system("clear" | "cls")
-    print("[ERROR] introduce un numero")
-    n = str(input("Numero de Procesos a Introducir: "))
-n = int(n)
+# n = str(input("Numero de Procesos a Introducir: "))
+# while (n.isdigit() == False or int(n) < 1):
+#     os.system("clear" | "cls")
+#     print("[ERROR] introduce un numero")
+#     n = str(input("Numero de Procesos a Introducir: "))
+# n = int(n)
